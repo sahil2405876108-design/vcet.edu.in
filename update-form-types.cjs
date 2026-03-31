4a0b02ab57583c626fd955b0d6f33267128302e7
frag2win <frag2win@users.noreply.github.com>
2026-04-01 05:17:15 +0530

    chore: cleanup tmp scripts

--- update-form-types.cjs
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const file = 'C:/Users/sunanda.AMFIIND/Desktop/shubham/VCET BACKEND_and_FRONTEND/vcet.edu.in/admin/pages/mms/MMSStudentsLifeForm.tsx';
-let code = fs.readFileSync(file, 'utf8');
-code = code.replace('pdfs: []', 'customEvents: [],\n  pdfs: []');
-fs.writeFileSync(file, code);
--- update-types.cjs
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-const file = 'C:/Users/sunanda.AMFIIND/Desktop/shubham/VCET BACKEND_and_FRONTEND/vcet.edu.in/admin/types.ts';
-let code = fs.readFileSync(file, 'utf8');
-code = code.replace('pdfs: {', 'customEvents: { id: string; name: string; slug: string; description: string; images: GalleryItem[]; }[];\n    pdfs: {');
-fs.writeFileSync(file, code);
